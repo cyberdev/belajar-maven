2cd113d598f3bd6635b75819add7f25967984f74
Nasrul Fatoni <nasrul.fatoni@gmail.com>
2018-08-02 11:00:35 +0700

    test

--- src/main/java/com/arulsoft/belajar/DemoJodaTime.java
+++ src/main/java/com/arulsoft/belajar/DemoJodaTime.java
@@ -2,6 +2,6 @@ package com.arulsoft.belajar;
 import org.joda.time.DateTime;
 public class DemoJodaTime{
 	public static void main(String[] args){
-		System.out.println("hello arulsssd");
+		System.out.println("hello arul");
 	}
 }
